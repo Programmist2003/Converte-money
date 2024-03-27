86698ee12213ecc1c4e16373734c56be621ae72d
Uliana <159609886+Programmist2003@users.noreply.github.com>
2024-03-27 16:17:02 +0300

    Add files via upload

---
+++ Converter-Money/Program.cs
@@ -0,0 +1,5 @@
+﻿int Countrubles = 300;
+int Counteuoros = Countrubles ^ 100;
+int Countdollars = Countrubles ^ Counteuoros ^ 92;
+
+Console.WriteLine("On your account " + Counteuoros + " Euoros " + Countdollars + " dollars ");
